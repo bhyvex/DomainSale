c361e286aff7cd6811759e3bca8141fbcf54bbdb
Dan Jellz <dan.jellesma@gmail.com>
2013-08-11 11:27:34 -0400

    Twitter Bootstrap and recaptcha added

--- index.php
+++ index.php
@@ -1,11 +1,45 @@
-<h2><?php print_r($_SERVER['HTTP_HOST']); ?> is for sale!</h2>
-<p>Contact us today to make an offer!</p>
+<?php
+include_once('header.php');
+?>
+    <script type="text/javascript">
+        var RecaptchaOptions = {
+            theme: 'clean'
+        };
+    </script>
 
-<form action="mail.php" method="POST">
-<p>Name</p> <input type="text" name="name">
-<p>Email</p> <input type="text" name="email">
-<p>Message</p><textarea name="message" rows="6" cols="25"></textarea><br />
-<input type="submit" value="Send"><input type="reset" value="Clear">
-</form>
+    <div class="container">
+        <h2><?php print_r($_SERVER['HTTP_HOST']); ?> is for sale!</h2>
 
-<?php print_r($_SERVER) ?>
+        <p>Contact us today to make an offer!</p>
+
+        <form action="mail.php" method="POST" class="form-horizontal">
+            <fieldset>
+                <label for="name">Name</label>
+                <input type="text" name="name" placeholder="name">
+
+                <p>Email</p> <input type="text" name="email" placeholder="youremail@domain.com">
+
+                <p>Phone</p> <input type="text" name="phone" placeholder="234-555-1212">
+                <br/>
+
+                <p>Message</p>
+                <textarea name="message" rows="6" cols="25">
+                </textarea><br/>
+
+                <br>
+                <?php
+                require_once('recaptchalib.php');
+                $publickey = "6LfH9OUSAAAAAF4v7_s39f9EA8s4xwILPOog2jAx"; // you got this from the signup page
+                echo recaptcha_get_html($publickey);
+                ?>
+                <input type="submit" value="Send" class="btn btn-primary">
+                <input type="reset" value="Clear" class="btn btn-danger">
+            </fieldset>
+        </form>
+    </div>
+
+
+
+<?php
+include_once('footer.php');
+?>
--- mail.php
+++ mail.php
@@ -1,14 +1,30 @@
-<?php 
+<?php
+require_once('recaptchalib.php');
+$privatekey = "6LfH9OUSAAAAAPECqogcP1KF7eM_fCh9ViCtYVES";
+$resp = recaptcha_check_answer($privatekey,
+    $_SERVER["REMOTE_ADDR"],
+    $_POST["recaptcha_challenge_field"],
+    $_POST["recaptcha_response_field"]);
 
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$message = $_POST['message'];
-	$formcontent="From: $name \n Message: $message";
-	$recipient = $_SERVER['SERVER_ADMIN'];
-	$subject = "Contact Form";
-	$mailheader = "From: $email \r\n";
-	mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-	echo "Thank You!";
+if (!$resp->is_valid) {
+    // What happens when the CAPTCHA was entered incorrectly
+    die ("The reCAPTCHA wasn't entered correctly. Go back and try it again." .
+        "(reCAPTCHA said: " . $resp->error . ")");
+} else {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    $formcontent = "From: $name \n Email: $email \n Phone: $phone \n
+	Message: $message";
+    $recipient = $_SERVER['SERVER_ADMIN'];
+    $subject = "Contact Form";
+    $mailheader = "From: $email \r\n";
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+    echo "Thank You for your consideration! We will be in touch regarding the sale of" . $_SERVER['HTTP_HOST'];
+}
+
+
 
 ?>
  
